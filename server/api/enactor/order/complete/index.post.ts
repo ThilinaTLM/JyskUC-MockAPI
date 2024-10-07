806358e57b3d5a120dbdb915d66e48729c336a62
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2024-10-07 17:25:33 +0530

    Update order complete logs

--- server/api/enactor/order/complete/index.post.ts
+++ server/api/enactor/order/complete/index.post.ts
@@ -1,7 +1,7 @@
 
 export default defineEventHandler(async (event ) => {
   const form: any = await readBody(event)
-  console.log(`Order Complete. @type:${form?.transaction?.['@type']} source:${form?.transaction?.source}`)
+  console.log(`[INFO] Order Complete. @type:${form?.payload?.transaction?.['@type']} source:${form?.payload?.transaction?.source} transactionId:${form?.payload?.transaction?.transactionId}`)
   console.debug(`[DEBUG] Order Complete Request Body`, form)
   setResponseStatus(event, 200)
   return {
