27ac713ef78059766f6d6120b9f4d30fc6b85374
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2024-10-07 17:11:31 +0530

    Simplify logs in complete order

--- server/api/enactor/order/complete/index.post.ts
+++ server/api/enactor/order/complete/index.post.ts
@@ -1,7 +1,8 @@
 
 export default defineEventHandler(async (event ) => {
   const form: any = await readBody(event)
-  console.log("Requested for completing order. Request Body:", form)
+  console.log(`Order Complete. @type:${form?.transaction?.['@type']} source:${form?.transaction?.source}`)
+  console.debug(`[DEBUG] Order Complete Request Body`, form)
   setResponseStatus(event, 200)
   return {
     "success": true,
