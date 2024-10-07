e13039e8f2ce12a3e37bf6e15bcfcbebc7bc772a
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2024-10-07 11:55:33 +0000

    Update order complete logs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -14,5 +15,5 @@ export default {
   plugins: [
     require('@tailwindcss/typography'),
   ],
-}
+};
 
